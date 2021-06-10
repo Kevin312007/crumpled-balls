6950f0de134a38216c5dfd02a4c59501559f2073
Kevin312007 <79404476+Kevin312007@users.noreply.github.com>
2021-06-10 18:39:01 +0800

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class paper 
+{
+
+    constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.paperWidth=200;
+		this.paperHeight=213;
+		this.wallThickness=20;
+		
+		this.image=loadImage("paper.png")
+
+
+
+
+
+
+
+
+
+
+}
