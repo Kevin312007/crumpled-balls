20d0e8829e908a3f5285b439265776869876d362
Kevin312007 <79404476+Kevin312007@users.noreply.github.com>
2021-06-13 20:24:25 +0800

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,15 +1,22 @@
-class paper 
+class Paper 
 {
 
     constructor(x,y)
 	{
 		this.x=x;
 		this.y=y;
-		this.paperWidth=200;
-		this.paperHeight=213;
-		this.wallThickness=20;
+		this.paperWidth=60;
+		this.paperHeight=60;
+		this.body=Bodies.circle(x,y,60)
+		World.add(world,this.body)
+		
 		
 		this.image=loadImage("paper.png")
+    }
+	display(){
+		imageMode(CENTER)
+		image(this.image,this.body.position.x,this.body.position.y,this.paperWeight,this.paperHeight)
+	}
 
 
 
@@ -21,3 +28,10 @@ class paper
 
 
 }
+
+
+
+
+
+
+
